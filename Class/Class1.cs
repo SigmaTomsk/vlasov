24362d22c243c73377595975c07b19b7eb1ee484
Roman Vlasov <klassroma2003@gmail.com>
2019-05-12 12:55:27 +0700

    Version 0.1

---
+++ Class/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Class2
+    {
+        public int plus(int[] a)
+        {
+            int rez = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                rez += a[i];
+
+            }
+            return rez;
+        }
+
+    }
+    
+}
